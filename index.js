a420f800894e3fb63615bb082f97aab723ba0734
CoohCooh <a04team001@gmail.com>
2023-12-17 20:45:22 -0300

    v0.0.2

--- index.js
+++ index.js
@@ -336,7 +336,7 @@ async function connectToWhatsApp() {
 
                         const user1Avatar = await cooh.profilePictureUrl(`${args[0].slice(1)}@c.us`)
                         if(!user1Avatar || user1Avatar == "item-not-found") user1Avatar = "https://coohzitos.tixte.co/r/sem-imagem-avatar.png"
-                        const user2Avatar = await cooh.profilePictureUrl(`${sender.split("@")[0]}`)
+                        const user2Avatar = await cooh.profilePictureUrl(`${sender.split("@")[0]}@c.us`)
                         if(!user2Avatar || user2Avatar == "item-not-found") user2Avatar = "https://coohzitos.tixte.co/r/sem-imagem-avatar.png"
 
                         const shipPercentage = Math.floor(Math.random() * 105)

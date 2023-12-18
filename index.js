101ef8a86c28343bb3c8d3cd403232d966c08fc0
CoohCooh <a04team001@gmail.com>
2023-12-17 21:12:38 -0300

    v0.0.2

--- index.js
+++ index.js
@@ -334,10 +334,10 @@ async function connectToWhatsApp() {
 
                         console.log(sender)
 
-                        const user1Avatar = "item-not-found" // await cooh.profilePictureUrl(`${args[0].slice(1)}@s.whatsapp.net`, "image")
-                        if(!user1Avatar || user1Avatar == "item-not-found") user1Avatar = "https://coohzitos.tixte.co/r/sem-imagem-avatar.png"
-                        const user2Avatar = "item-not-found" //await cooh.profilePictureUrl(`${sender}`, "image")
-                        if(!user2Avatar || user2Avatar == "item-not-found") user2Avatar = "https://coohzitos.tixte.co/r/sem-imagem-avatar.png"
+                        const user1Avatar = "https://coohzitos.tixte.co/r/sem-imagem-avatar.png" // await cooh.profilePictureUrl(`${args[0].slice(1)}@s.whatsapp.net`, "image")
+                        //if(!user1Avatar || user1Avatar == "item-not-found") user1Avatar = "https://coohzitos.tixte.co/r/sem-imagem-avatar.png"
+                        const user2Avatar = "https://coohzitos.tixte.co/r/sem-imagem-avatar.png" //await cooh.profilePictureUrl(`${sender}`, "image")
+                        //if(!user2Avatar || user2Avatar == "item-not-found") user2Avatar = "https://coohzitos.tixte.co/r/sem-imagem-avatar.png"
 
                         const shipPercentage = Math.floor(Math.random() * 105)
 
@@ -404,10 +404,10 @@ async function connectToWhatsApp() {
                         //const user1Avatar = await cooh.profilePictureUrl(`${args[0].slice(1)}@c.us`, 'image')
                         //const user2Avatar = await cooh.profilePictureUrl(`${args[1].slice(1)}@c.us`, 'image')
 
-                        const user1Avatar = "item-not-found" // await cooh.profilePictureUrl(`${args[0].slice(1)}@s.whatsapp.net`, "image")
-                        if(!user1Avatar || user1Avatar == "item-not-found") user1Avatar = "https://coohzitos.tixte.co/r/sem-imagem-avatar.png"
-                        const user2Avatar = "item-not-found" //await cooh.profilePictureUrl(`${args[1].slice(1)}@c.us`, "image")
-                        if(!user2Avatar || user2Avatar == "item-not-found") user2Avatar = "https://coohzitos.tixte.co/r/sem-imagem-avatar.png"
+                        const user1Avatar = "https://coohzitos.tixte.co/r/sem-imagem-avatar.png" // await cooh.profilePictureUrl(`${args[0].slice(1)}@s.whatsapp.net`, "image")
+                        //if(!user1Avatar || user1Avatar == "item-not-found") user1Avatar = "https://coohzitos.tixte.co/r/sem-imagem-avatar.png"
+                        const user2Avatar = "https://coohzitos.tixte.co/r/sem-imagem-avatar.png" //await cooh.profilePictureUrl(`${args[1].slice(1)}@c.us`, "image")
+                        //if(!user2Avatar || user2Avatar == "item-not-found") user2Avatar = "https://coohzitos.tixte.co/r/sem-imagem-avatar.png"
 
 
                         const shipPercentage = Math.floor(Math.random() * 105)

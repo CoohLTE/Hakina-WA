98c452332deb22dbe4b4084f1961aa2ad005006a
Cooh Cooh <saiqvo495@gmail.com>
2024-04-10 09:09:03 -0300

    v1.0.0

--- index.js
+++ index.js
@@ -399,10 +399,10 @@ async function connectToWhatsApp() {
 
                 case "download":
                     if(!isGroup) return enviar(resposta.grupo)
-                    console.log(info.message.extendedTextMessage.contextInfo.quotedMessage)
-                    const textInformationURLDownload = info.message.extendedTextMessage.contextInfo.quotedMessage.extendedTextMessage.text
+                    //console.log(info.message.extendedTextMessage.contextInfo.quotedMessage)
+                    const textInformationURLDownload = info.message.extendedTextMessage.contextInfo.quotedMessage.conversation
                     if(!isUrl(q) || !isUrl(textInformationURLDownload)) return enviar(`Link Invalido! Marque Ou Coloque O Link Que Deseja Enviar!`)
-                    if(info.message.extendedTextMessage.contextInfo.quotedMessage.extendedTextMessage.text.includes("mediafire.com/file/")) {
+                    if(info.message.extendedTextMessage.contextInfo.quotedMessage.extendedTextMessage.conversation.includes("mediafire.com/file/")) {
                         await fetch(`https://tohka.tech/api/dl/mediafire?link=${textInformationURLDownload}&apikey=KzqKxVmU65`).then((api) => api.json()).then((json) => {
                             if(json.status != "operando") return enviar("Link invalido ou a API está offline! Tente novamente...")
                             const tamanhoDownload = json.resultado.tamanho

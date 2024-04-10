f8107e18a70fd6628dc285fedadd31c048d798ef
Cooh Cooh <saiqvo495@gmail.com>
2024-04-10 09:09:56 -0300

    v1.0.0

--- index.js
+++ index.js
@@ -401,7 +401,7 @@ async function connectToWhatsApp() {
                     if(!isGroup) return enviar(resposta.grupo)
                     //console.log(info.message.extendedTextMessage.contextInfo.quotedMessage)
                     const textInformationURLDownload = info.message.extendedTextMessage.contextInfo.quotedMessage.conversation
-                    if(!isUrl(q) || !isUrl(textInformationURLDownload)) return enviar(`Link Invalido! Marque Ou Coloque O Link Que Deseja Enviar!`)
+                    if(isUrl(q) || isUrl(textInformationURLDownload)) return enviar(`Link Invalido! Marque Ou Coloque O Link Que Deseja Enviar!`)
                     if(info.message.extendedTextMessage.contextInfo.quotedMessage.extendedTextMessage.conversation.includes("mediafire.com/file/")) {
                         await fetch(`https://tohka.tech/api/dl/mediafire?link=${textInformationURLDownload}&apikey=KzqKxVmU65`).then((api) => api.json()).then((json) => {
                             if(json.status != "operando") return enviar("Link invalido ou a API está offline! Tente novamente...")

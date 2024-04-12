8ad69aae86b19420c2e78880ed397f41d3e0494d
Cooh Cooh <saiqvo495@gmail.com>
2024-04-12 09:47:02 -0300

    v1.0.0

--- index.js
+++ index.js
@@ -487,7 +487,7 @@ async function connectToWhatsApp() {
 
                         const user1Avatar = await cooh.profilePictureUrl(`${args[0].slice(1)}@c.us`) == "item-not-found" ? "http://storage.cdworld.cloud/r/sem-imagem-avatar.png" : await cooh.profilePictureUrl(`${args[0].slice(1)}@c.us`)
 
-                        const user2Avatar = await cooh.profilePictureUrl(`${sender.split("@")[0]}@c.us`) == "item-not-found" ? "http://storage.cdworld.cloud/r/sem-imagem-avatar.png" : await cooh.profilePictureUrl(`${sender.split("@")[0]}@c.us`)
+                        const user2Avatar = await cooh.profilePictureUrl(`${sender.split("@")[0]}@c.us`) != "item-not-found" ? await cooh.profilePictureUrl(`${sender.split("@")[0]}@c.us`) : "http://storage.cdworld.cloud/r/sem-imagem-avatar.png"
 
                         const shipPercentage = Math.floor(Math.random() * 105)
 
